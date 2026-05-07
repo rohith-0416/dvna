42bca103c3b8f55246a731bcfa672711ebd5786a
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-07 19:40:45 +0530

    fix(security): Missing input validation for APP_LISTEN
    Fixes CWE-22 in config/server.js

--- config/server.js
+++ config/server.js
@@ -1,4 +1,4 @@
 module.exports = {
-    listen: process.env.APP_LISTEN || '0.0.0.0',
+    listen: (process.env.APP_LISTEN || '0.0.0.0').match(/^(\d{1,3}\.){3}\d{1,3}$/) ? process.env.APP_LISTEN : '0.0.0.0',
     port: process.env.APP_PORT || process.env.PORT || 9090
 }
