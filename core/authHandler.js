4a0a83501e377710f63a6ff3ae3e8af17b60dba2
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-07 19:41:05 +0530

    fix(security): Missing Input Validation
    Fixes CWE-20 in core/authHandler.js

--- core/authHandler.js
+++ core/authHandler.js
@@ -3,7 +3,7 @@ var bCrypt = require('bcrypt')
 var md5 = require('md5')
 
 module.exports.isAuthenticated = function (req, res, next) {
-	if (req.session && req.session.user && req.session.user.id && req.user && req.user.id && req.user.id === req.session.user.id) {
+	if (req.isAuthenticated()) {
 		req.flash('authenticated', true)
 		return next();
 	}
@@ -11,76 +11,57 @@ module.exports.isAuthenticated = function (req, res, next) {
 }
 
 module.exports.isNotAuthenticated = function (req, res, next) {
-	if (!req.session || !req.session.user || !req.session.user.id || !req.user || !req.user.id || req.user.id !== req.session.user.id)
+	if (!req.isAuthenticated())
 		return next();
 	res.redirect('/learn');
 }
 
 module.exports.forgotPw = function (req, res) {
-	if (req.body.login) {
-		db.User.find({
-			where: {
-				'login': req.body.login
-			}
-		}).then(user => {
-			if (user) {
-				// Send reset link via email happens here
-				req.flash('info', 'Check email for reset link')
-				res.redirect('/login')
-			} else {
-				req.flash('danger', "Invalid login username")
-				res.redirect('/forgotpw')
-			}
-		})
-	} else {
-		req.flash('danger', "Invalid login username")
-		res.redirect('/forgotpw')
-	}
-}
-
-module.exports.resetPw = function (req, res) {
-	if (req.query.login) {
-		db.User.find({
-			where: {
-				'login': req.query.login
-			}
-		}).then(user => {
-			if (user) {
-				if (req.query.token == md5(req.query.login)) {
-					res.render('resetpw', {
-						login: req.query.login,
-						token: req.query.token
-					})
-				} else {
-					req.flash('danger', "Invalid reset token")
-					res.redirect('/forgotpw')
-				}
-			} else {
-				req.flash('danger', "Invalid login username")
-				res.redirect('/forgotpw')
-			}
-		})
-	} else {
-		req.flash('danger', "Non Existant login username")
-		res.redirect('/forgotpw')
-	}
-}
-
-module.exports.resetPwSubmit = function (req, res) {
-	if (req.body.password && req.body.cpassword && req.body.login && req.body.token) {
-		if (req.body.password == req.body.cpassword) {
+	if (req.body && req.body.login) {
+		if (req.body.login.trim()) {
 			db.User.find({
 				where: {
 					'login': req.body.login
 				}
 			}).then(user => {
 				if (user) {
-					if (req.body.token == md5(req.body.login)) {
-						user.password = bCrypt.hashSync(req.body.password, bCrypt.genSaltSync(10), null)
-						user.save().then(function () {
-							req.flash('success', "Passowrd successfully reset")
-							res.redirect('/login')
-						})
+					// Send reset link via email happens here
+					req.flash('info', 'Check email for reset link')
+					res.redirect('/login')
+				} else {
+					req.flash('danger', "Invalid login username")
+					res.redirect('/forgotpw')
+				}
+			})
+		} else {
+			req.flash('danger', "Invalid login username")
+			res.redirect('/forgotpw')
+		}
+	} else {
+		req.flash('danger', "Invalid request")
+		res.redirect('/forgotpw')
+	}
+}
+
+module.exports.resetPw = function (req, res) {
+	if (req.query && req.query.login) {
+		if (req.query.login.trim()) {
+			db.User.find({
+				where: {
+					'login': req.query.login
+				}
+			}).then(user => {
+				if (user) {
+					if (req.query.token && req.query.token.trim()) {
+						if (req.query.token == md5(req.query.login)) {
+							res.render('resetpw', {
+								login: req.query.login,
+								token: req.query.token
+							})
+						} else {
+							req.flash('danger', "Invalid reset token")
+							res.redirect('/forgotpw')
+						}
 					} else {
 						req.flash('danger', "Invalid reset token")
 						res.redirect('/forgotpw')
@@ -91,11 +72,51 @@ module.exports.resetPwSubmit = function (req, res) {
 				}
 			})
 		} else {
-			req.flash('danger', "Passowords do not match")
-			res.render('resetpw', {
-				login: req.query.login,
-				token: req.query.token
-			})
+			req.flash('danger', "Non Existant login username")
+			res.redirect('/forgotpw')
+		}
+	} else {
+		req.flash('danger', "Invalid request")
+		res.redirect('/forgotpw')
+	}
+}
+
+module.exports.resetPwSubmit = function (req, res) {
+	if (req.body && req.body.password && req.body.cpassword && req.body.login && req.body.token) {
+		if (req.body.password.trim() && req.body.cpassword.trim() && req.body.login.trim() && req.body.token.trim()) {
+			if (req.body.password == req.body.cpassword) {
+				db.User.find({
+					where: {
+						'login': req.body.login
+					}
+				}).then(user => {
+					if (user) {
+						if (req.body.token == md5(req.body.login)) {
+							user.password = bCrypt.hashSync(req.body.password, bCrypt.genSaltSync(10), null)
+							user.save().then(function () {
+								req.flash('success', "Passowrd successfully reset")
+								res.redirect('/login')
+							})
+						} else {
+							req.flash('danger', "Invalid reset token")
+							res.redirect('/forgotpw')
+						}
+					} else {
+						req.flash('danger', "Invalid login username")
+						res.redirect('/forgotpw')
+					}
+				})
+			} else {
+				req.flash('danger', "Passowords do not match")
+				res.render('resetpw', {
+					login: req.query.login,
+					token: req.query.token
+				})
+			}
+
+		} else {
+			req.flash('danger', "Invalid request")
+			res.redirect('/forgotpw')
 		}
 
 	} else {
