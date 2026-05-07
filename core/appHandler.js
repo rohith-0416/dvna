6fa1c27eab64680fd8da0e67b0f9e4ffb7b2f74d
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-07 19:40:56 +0530

    fix(security): Unsafe Eval
    Fixes CWE-94 in core/appHandler.js

--- core/appHandler.js
+++ core/appHandler.js
@@ -1,50 +1,17 @@
-module.exports.userEditSubmit = function (req, res) {
-	db.User.find({
-		where: {
-			'id': req.body.id
-		}		
-	}).then(user =>{
-		if(req.body.password.length>0){
-			if(req.body.password.length>0){
-				if (req.body.password == req.body.cpassword) {
-					user.password = bCrypt.hashSync(req.body.password, bCrypt.genSaltSync(10), null)
-				}else{
-					req.flash('warning', 'Passwords dont match')
-					res.render('app/useredit', {
-						userId: req.user.id,
-						userEmail: req.user.email,
-						userName: req.user.name,
-					})
-					return		
-				}
-			}else{
-				req.flash('warning', 'Invalid Password')
-				res.render('app/useredit', {
-					userId: req.user.id,
-					userEmail: req.user.email,
-					userName: req.user.name,
-				})
-				return
-			}
-		}
-		if(req.user.id === req.body.id){ // added check
-			user.email = req.body.email
-			user.name = req.body.name
-			user.save().then(function () {
-				req.flash('success',"Updated successfully")
-				res.render('app/useredit', {
-					userId: req.body.id,
-					userEmail: req.body.email,
-					userName: req.body.name,
-				})
+module.exports.calc = function (req, res) {
+	if (req.body.eqn) {
+		try {
+			res.render('app/calc', {
+				output: mathjs.parse(req.body.eqn).toString()
 			})
-		} else {
-			req.flash('danger', 'Unauthorized to edit this user')
-			res.render('app/useredit', {
-				userId: req.user.id,
-				userEmail: req.user.email,
-				userName: req.user.name,
+		} catch (e) {
+			res.render('app/calc', {
+				output: 'Invalid math string: ' + e.message
 			})
 		}
-	})
+	} else {
+		res.render('app/calc', {
+			output: 'Enter a valid math string like (3+3)*2'
+		})
+	}
 }
