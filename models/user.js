0502c354cd807ec2d79bea778e3c14daeb5f248a
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-07 19:41:12 +0530

    fix(security): Missing input validation
    Fixes CWE-89 in models/user.js

--- models/user.js
+++ models/user.js
@@ -10,23 +10,42 @@ module.exports = function (sequelize, DataTypes) {
         name: {
             type: DataTypes.STRING,
             allowNull: false,
+            validate: {
+                notNull: true
+            }
         },
         login: {
             type: DataTypes.STRING,
             allowNull: false,
-            unique: true
+            unique: true,
+            validate: {
+                notNull: true,
+                is: /^[a-zA-Z0-9_]+$/
+            }
         },
         email: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notNull: true,
+                isEmail: true
+            }
         },
         password: {
             type: DataTypes.STRING,
-            allowNull: false
+            allowNull: false,
+            validate: {
+                notNull: true,
+                is: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+            }
         },
         role: {
             type: DataTypes.STRING,
             allowNull: true
+        }
+    }, {
+        validate: {
+            validateAtCreate: true
         }
     });
     return User;
