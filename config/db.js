db9ecc5f5a7eca5fc9171f11cd4ccda24fa3a059
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-07 19:40:43 +0530

    fix(security): Missing Input Validation
    Fixes CWE-20 in config/db.js

--- config/db.js
+++ config/db.js
@@ -3,6 +3,6 @@ module.exports = {
   password: process.env.MYSQL_PASSWORD || '',
   database: process.env.MYSQL_DATABASE || '',
   host: process.env.MYSQL_HOST || 'mysql-db',
-  port: process.env.MYSQL_PORT || 3306,
+  port: (process.env.MYSQL_PORT || 3306).trim(),
   dialect: 'mysql'
 }
