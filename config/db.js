0ac7d9a677b21bf9faa6cd7efe3198da26a5ee56
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-08 09:25:16 +0530

    fix(security): Hardcoded Secrets
    Fixes CWE-798 in config/db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,13 @@ module.exports = {
   username: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST || 'mysql-db',
-  port: process.env.MYSQL_PORT || 3306,
+  host: process.env.MYSQL_HOST,
+  port: process.env.MYSQL_PORT,
   dialect: 'mysql'
 }
+
+if (!process.env.MYSQL_HOST) throw new Error('MYSQL_HOST environment variable is not set');
+if (!process.env.MYSQL_PORT) process.env.MYSQL_PORT = 3306;
+if (!process.env.MYSQL_USER) throw new Error('MYSQL_USER environment variable is not set');
+if (!process.env.MYSQL_PASSWORD) throw new Error('MYSQL_PASSWORD environment variable is not set');
+if (!process.env.MYSQL_DATABASE) throw new Error('MYSQL_DATABASE environment variable is not set');
