3ffbda3707a55e4d99b4b1081dbc7324232448c2
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-08 09:25:32 +0530

    fix(security): Insecure database connection
    Fixes CWE-316 in models/index.js

--- models/index.js
+++ models/index.js
@@ -4,16 +4,15 @@ var fs = require("fs");
 var path = require("path");
 var Sequelize = require("sequelize");
 var env = process.env.NODE_ENV || "development";
-var config = {
-  database: process.env.DB_NAME,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  dialect: process.env.DB_DIALECT
-}
+var config = require("../config/db.js")
 
-if (process.env.DATABASE_URL) {
-  var sequelize = new Sequelize(process.env.DATABASE_URL);
+if (process.env.DB_HOST && process.env.DB_USER && process.env.DB_PASSWORD && process.env.DB_NAME) {
+  var sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT
+  });
+} else if (process.env.DATABASE_URL) {
+  var sequelize = new Sequelize(process.env.DATABASE_URL.replace(/:\w+@/, ':***@').replace(/:\w+(?=\/)/, ':***'));
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
