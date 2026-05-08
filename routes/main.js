5ec165796ed428c8bf92b53d503d8337ebc57b7b
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-08 09:25:42 +0530

    fix(security): Potential Path Traversal
    Fixes CWE-22 in routes/main.js

--- routes/main.js
+++ routes/main.js
@@ -12,25 +12,25 @@ module.exports = function (passport) {
 	})
 
 	router.get('/learn/vulnerability/:vuln', authHandler.isAuthenticated, function (req, res) {
-		if (!vulnDict[req.params.vuln]) {
+		if (vulnDict[req.params.vuln]) {
+			res.render('vulnerabilities/layout', {
+				vuln: req.params.vuln,
+				vuln_title: vulnDict[req.params.vuln],
+				vuln_scenario: req.params.vuln + '/scenario',
+				vuln_description: req.params.vuln + '/description',
+				vuln_reference: req.params.vuln + '/reference',
+				vulnerabilities:vulnDict
+			}, function (err, html) {
+				if (err) {
+					console.log(err)
+					res.status(404).send('404')
+				} else {
+					res.send(html)
+				}
+			})
+		} else {
 			res.status(404).send('404')
-			return
 		}
-		res.render('vulnerabilities/layout', {
-			vuln: req.params.vuln,
-			vuln_title: vulnDict[req.params.vuln],
-			vuln_scenario: req.params.vuln + '/scenario',
-			vuln_description: req.params.vuln + '/description',
-			vuln_reference: req.params.vuln + '/reference',
-			vulnerabilities:vulnDict
-		}, function (err, html) {
-			if (err) {
-				console.log(err)
-				res.status(404).send('404')
-			} else {
-				res.send(html)
-			}
-		})
 	})
 
 	router.get('/learn', authHandler.isAuthenticated, function (req, res) {
