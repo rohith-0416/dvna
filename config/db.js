bc22d8b1bb68cd39596107a98de9673257747065
B Rohith Jayaraman <127156018@sastra.ac.in>
2026-05-08 09:25:18 +0530

    fix(security): Missing Input Validation
    Fixes CWE-20 in config/db.js

--- config/db.js
+++ config/db.js
@@ -1,14 +1,8 @@
 module.exports = {
-  username: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-  host: process.env.MYSQL_HOST,
-  port: process.env.MYSQL_PORT,
+  username: process.env.MYSQL_USER ? process.env.MYSQL_USER : '',
+  password: process.env.MYSQL_PASSWORD ? process.env.MYSQL_PASSWORD : '',
+  database: process.env.MYSQL_DATABASE ? process.env.MYSQL_DATABASE : '',
+  host: process.env.MYSQL_HOST || 'mysql-db',
+  port: process.env.MYSQL_PORT || 3306,
   dialect: 'mysql'
 }
-
-if (!process.env.MYSQL_HOST) throw new Error('MYSQL_HOST environment variable is not set');
-if (!process.env.MYSQL_PORT) process.env.MYSQL_PORT = 3306;
-if (!process.env.MYSQL_USER) throw new Error('MYSQL_USER environment variable is not set');
-if (!process.env.MYSQL_PASSWORD) throw new Error('MYSQL_PASSWORD environment variable is not set');
-if (!process.env.MYSQL_DATABASE) throw new Error('MYSQL_DATABASE environment variable is not set');
